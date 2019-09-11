79b609b8b10a44e5d2274f09ab387c728ef8e9d9
newbiechen <newbiechen@gmail.com>
2019-09-11 17:22:13 +0800

    change:创建 c++ 环境

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_newbiechen_nbreader_ui_page_bookshelf_BookShelfFragment_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string value = "hello wolrd";
+    return env->NewStringUTF(value.c_str());
+}
--- app/src/main/java/com/example/newbiechen/nbreader/ui/page/bookshelf/BookShelfFragment.kt
+++ app/src/main/java/com/example/newbiechen/nbreader/ui/page/bookshelf/BookShelfFragment.kt
@@ -10,14 +10,32 @@ import com.example.newbiechen.nbreader.ui.page.read.ReadActivity
 import com.youtubedl.ui.main.base.BaseBindingFragment
 
 class BookShelfFragment : BaseBindingFragment<FragmentBookShelfBinding>() {
+
+
     override fun initContentView(): Int = R.layout.fragment_book_shelf
 
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    external fun test(): String
+
     companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+
         fun newInstance() = BookShelfFragment()
     }
 
     override fun initView() {
         super.initView()
+
+        mDataBinding.tvRead.text = stringFromJNI()
 
         mDataBinding.tvRead.setOnClickListener {
             startActivity(ReadActivity::class)
